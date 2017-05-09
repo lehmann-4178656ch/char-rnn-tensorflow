213bd0d69cdec5e5917cd3ae3e5aa6f32f464e4c
Kuraron <kuraron.kuratan@gmail.com>
2017-05-09 09:03:19 +0200

    Change to json config

--- demo.py
+++ demo.py
@@ -17,40 +17,38 @@ def main():
                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--save_dir', type=str, default='save',
                         help='model directory to store checkpointed models')
-    parser.add_argument('-n', type=int, default=500,
-                        help='number of characters to sample')
-    parser.add_argument('--prime', type=text_type, default=u' ',
-                        help='prime text')
-    parser.add_argument('--sample', type=int, default=1,
-                        help='0 to use max at each timestep, 1 to sample at '
-                             'each timestep, 2 to sample on spaces')
 
     args = parser.parse_args()
-    sample(args)
-
-
-def sample(data):
-    text = ''
-    args = json.loads(data.decode("utf-8"))
-    with open(os.path.join(args['save_dir'], 'config.pkl'), 'rb') as f:
+    with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
         saved_args = cPickle.load(f)
-    with open(os.path.join(args['save_dir'], 'chars_vocab.pkl'), 'rb') as f:
+    with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'rb') as f:
         chars, vocab = cPickle.load(f)
     model = Model(saved_args, training=False)
-    with tf.Session() as sess:
-        tf.global_variables_initializer().run()
-        saver = tf.train.Saver(tf.global_variables())
-        ckpt = tf.train.get_checkpoint_state(args['save_dir'])
-        if ckpt and ckpt.model_checkpoint_path:
-            saver.restore(sess, ckpt.model_checkpoint_path)
-            text = model.sample(sess, chars, vocab, int(args['n']), args['prime'],
-                               int(args['sample']))
-    return json.dumps({
-        'text': text,
-    }).encode("utf-8")
+
+    def run(data):
+        data = json.loads(data.decode("utf-8"))
+        with tf.Session() as sess:
+            tf.global_variables_initializer().run()
+            saver = tf.train.Saver(tf.global_variables())
+            ckpt = tf.train.get_checkpoint_state(args.save_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                text = model.sample(sess, chars, vocab, int(data['n']), data['prime'],
+                                   int(data['sample']))
+        return json.dumps({
+            'text': text,
+        }).encode("utf-8")
+    return run
 
 if __name__ == '__main__':
-    sample('{"n": 500, "save_dir": "./save", "sample": 1, "prime": " "}'.encode('utf-8'))
-    sample('{"n": 500, "save_dir": "./save", "sample": 1, "prime": " "}'.encode('utf-8'))
+    run = main()
+    data = '{"n": 500, "sample": 0, "prime": " "}'.encode('utf-8')
+    print(run(data).decode('utf-8'))
+    print()
+    data = '{"n": 500, "sample": 1, "prime": " "}'.encode('utf-8')
+    print(run(data).decode('utf-8'))
+    print()
+    data = '{"n": 500, "sample": 2, "prime": " "}'.encode('utf-8')
+    print(run(data).decode('utf-8'))
     #riseml.serve(sample)
 
